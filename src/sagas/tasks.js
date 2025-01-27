522dcbaefa677a03c6af070e348660fc3c32dadd
Venkat varma <kvenkatvarma@gmail.com>
2025-01-27 18:14:46 +0530

    http requests

--- src/sagas/tasks.js
+++ src/sagas/tasks.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+import * as actionTypes from "../constants/action-types";
+import { put } from "redux-saga/effects";
+
+
 export const fetchTasksWorkerSaga =function*(){
-    console.log("Worker");
+   
+    yield put({type:actionTypes.FETCH_TASKS_PENDING});
+    try
+    {
+        let response = yield axios.get("http://localhost:7000/tasks");
+        yield put({type:actionTypes.FETCH_TASKS_FULFILLED,payload:response});     
+    }
+    catch(error)
+    {
+        yield put({type:actionTypes.FETCH_TASKS_REJECTED,payload:error});     
+    }
 };
