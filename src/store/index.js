3cbb0b7a6a8fb3fc7f9c14bd8a3980e1fb5bf3a2
Venkat varma <kvenkatvarma@gmail.com>
2025-01-27 16:13:17 +0530

    redux-saga and root saga

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,13 @@ import allReducers from "../reducers";
 import {thunk} from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reduxPromiseMiddleware from "redux-promise-middleware";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "../sagas";
 
-var store = createStore(allReducers,composeWithDevTools(applyMiddleware(thunk,reduxPromiseMiddleware)));
+let saga = createSagaMiddleware();
+let middleware =[thunk,saga,reduxPromiseMiddleware];
+
+var store = createStore(allReducers,{},composeWithDevTools(applyMiddleware(...middleware)));
+//invoke root saga
+saga.run(rootSaga);
 export default store;      
