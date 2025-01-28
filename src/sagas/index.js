69944c1714f6b103f544d2a3640517bc40e62668
Venkat varma <kvenkatvarma@gmail.com>
2025-01-28 16:35:29 +0530

    all() effect

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,5 @@
 import {fetchTasksWorkerSaga,createTasksWorkerSaga,deleteTasksWorkerSaga} from "./tasks.js";
-import { takeEvery,debounce, fork, throttle } from "redux-saga/effects";
+import { takeEvery,debounce, fork, throttle,all } from "redux-saga/effects";
 import * as actionTypes from "../constants/action-types";
 
 export const tasksWatcherSaga = function*(){
@@ -16,6 +16,9 @@ export const employeesWatcherSaga = function*(){
 
 export const rootSaga = function*(){
     console.log("rootsaga invoked");
-    yield fork(tasksWatcherSaga);
-    yield fork(employeesWatcherSaga);
+    yield all([
+        fork(tasksWatcherSaga),
+        fork(employeesWatcherSaga)
+    ]);
+       
 };
