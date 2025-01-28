d9222812d7167e6845da45984a5f79f0f059cf69
Venkat varma <kvenkatvarma@gmail.com>
2025-01-28 16:45:53 +0530

    take()

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,13 @@
 import {fetchTasksWorkerSaga,createTasksWorkerSaga,deleteTasksWorkerSaga} from "./tasks.js";
-import { takeEvery,debounce, fork, throttle,all } from "redux-saga/effects";
+import { takeEvery,debounce, fork, throttle,all,take } from "redux-saga/effects";
 import * as actionTypes from "../constants/action-types";
 
 export const tasksWatcherSaga = function*(){
     yield takeEvery(actionTypes.FETCH_TASKS,fetchTasksWorkerSaga);
 
     yield throttle(1000 * 30,actionTypes.CREATE_TASK,createTasksWorkerSaga);
+
+    yield take(actionTypes.CREATE_TASK);
 
     yield takeEvery(actionTypes.DELETE_TASK,deleteTasksWorkerSaga);
 };
