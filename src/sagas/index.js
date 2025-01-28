eb491a4c89a198854c7693b6c0c234bfefd647fa
Venkat varma <kvenkatvarma@gmail.com>
2025-01-28 11:28:26 +0530

    debounce

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,12 +1,12 @@
 import {fetchTasksWorkerSaga,createTasksWorkerSaga,deleteTasksWorkerSaga} from "./tasks.js";
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery,debounce } from "redux-saga/effects";
 import * as actionTypes from "../constants/action-types";
 
 export const rootSaga = function*(){
     console.log("rootsaga invoked");
      yield takeEvery(actionTypes.FETCH_TASKS,fetchTasksWorkerSaga);
 
-     yield takeEvery(actionTypes.CREATE_TASK,createTasksWorkerSaga);
+     yield debounce(1000,actionTypes.CREATE_TASK,createTasksWorkerSaga);
 
      yield takeEvery(actionTypes.DELETE_TASK,deleteTasksWorkerSaga);
 
